395757837d916a7078f2b4a340eb28c9c09e2e12
Juliano Ugalde <julianougalde@gmail.com>
2021-10-14 15:28:23 -0300

    Enviando arquivos

---
+++ src/ControleAcesso/Dao/DaoBase.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace ControleAcesso.Dao
+{
+    public abstract class DaoBase: IDisposable
+    {
+        protected readonly SqlConnection con;
+
+        protected DaoBase()
+        {
+            con = new SqlConnection(@"Data Source=UGALDE\SQLEXPRESS;Initial Catalog=StarWars-EstrelaDaMorte;Integrated Security=True;Connect Timeout=30");
+        }
+
+        protected async Task Insert(string comando)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(comando, con);
+            await cmd.ExecuteNonQueryAsync();
+            con.Close();
+        }
+
+        protected async Task Select(string comando, Action<SqlDataReader> tratamentoLeitura)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand(comando, con);
+            SqlDataReader dr = await cmd.ExecuteReaderAsync();
+            tratamentoLeitura(dr);
+            con.Close();
+        }
+
+        public void Dispose()
+        {
+            con?.Close();
+            con?.Dispose();
+        }
+    }
+}
